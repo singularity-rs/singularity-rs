d17937dc4988feb20e8bb0cb035bc7dadffef3cf
Felix Karg <f.karg10@gmail.com>
2019-10-12 15:57:41 +0200

    split the original files apart quite a bit
    Similar to states_ui example from amethyst.

---
+++ src/welcome.rs
@@ -0,0 +1,52 @@
+use amethyst::{
+    ecs::prelude::Entity,
+    input::{is_close_requested, is_key_down, is_mouse_button_down},
+    prelude::*,
+    ui::UiCreator,
+    winit::{MouseButton, VirtualKeyCode},
+};
+
+use log::info;
+
+use crate::util::delete_hierarchy;
+
+#[derive(Default, Debug)]
+pub struct WelcomeScreen {
+    ui_handle: Option<Entity>,
+}
+
+impl SimpleState for WelcomeScreen {
+    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        let world = data.world;
+
+        self.ui_handle =
+            Some(world.exec(|mut creator: UiCreator<'_>| creator.create("ui/welcome.ron", ())));
+    }
+
+    fn handle_event(
+        &mut self,
+        _: StateData<'_, GameData<'_, '_>>,
+        event: StateEvent,
+    ) -> SimpleTrans {
+        match &event {
+            StateEvent::Window(event) => {
+                if is_close_requested(&event) || is_key_down(&event, VirtualKeyCode::Escape) {
+                    Trans::Quit
+                } else if is_mouse_button_down(&event, MouseButton::Left) {
+                    info!("Switching to MainMenu!");
+                    Trans::Switch(Box::new(crate::menu::MainMenu::default()))
+                } else {
+                    Trans::None
+                }
+            }
+            _ => Trans::None,
+        }
+    }
+
+    fn on_stop(&mut self, data: StateData<GameData>) {
+        if let Some(handler) = self.ui_handle {
+            delete_hierarchy(handler, data.world).expect("Failed to remove WelcomeScreen");
+        }
+        self.ui_handle = None;
+    }
+}
